51ad5ba17d76e330829f4d01a13536005a624312
bilalmukhtiar5 <billuedwardian08@gmail.com>
2023-09-25 19:26:18 +0500

    updated

--- script.js
+++ script.js
@@ -32,18 +32,37 @@ let stuCGPA = 3.8
 
 */
 
+/*
 
-
-let studentNames = {
+let studentNames = [
     'Bilal',
     'Saima',
     'Sana',
     'Chris',
     'Daim',
 
-};
-let studentID = {1,2,3,4,5};
-let studentCGPA = {3.2,3.1,3.0,2.7,3.0};
-let studentAddress = {"lahore",'peshawar','Islamabad','lahore','lahore'};
+];
+let studentID = [1,2,3,4,5];
+let studentCGPA = [3.2,3.1,3.0,2.7,3.0];
+let studentAddress = ["lahore",'peshawar','Islamabad','lahore','lahore'];
 
-console.log(studentNames);
+studentNames.push("Simon")
+
+let student1 = studentNames[0];
+console.log(student1)
+
+studentNames[4] = "tariq";
+console.log(studentNames[4])
+
+*/
+
+//task 1 
+//Write a program that prompts the user to enter five numbers and stores
+//them in an array. Then, output the sum of the numbers to the console
+
+
+let numbers = [
+     
+]
+
+let number1 = Number("Enter number one", number1)
