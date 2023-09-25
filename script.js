a7e36ab95c030c0ae4bb96bc339715c7d0910ed0
bilalmukhtiar5 <billuedwardian08@gmail.com>
2023-09-25 18:40:34 +0500

    updated

---
+++ script.js
@@ -0,0 +1,49 @@
+// Arrays
+/*
+//student 1
+let stu1 = 'sajid'
+let stuAddress = 'lahore'
+let stuID = 1
+let stuCGPA = 3.8
+
+//student2 
+let stu1 = 'sajid'
+let stuAddress = 'lahore'
+let stuID = 2
+let stuCGPA = 3.8
+
+//student 3
+let stu1 = 'sajid'
+let stuAddress = 'pindi'
+let stuID = 3
+let stuCGPA = 3.8
+
+//student 4 
+let stu1 = 'tashfeen'
+let stuAddress = 'islamabad'
+let stuID = 4
+let stuCGPA = 3.8
+
+//student 5
+let stu1 = 'moon'
+let stuAddress = 'peshawar'
+let stuID = 5
+let stuCGPA = 3.8
+
+*/
+
+
+
+let studentNames = {
+    'Bilal',
+    'Saima',
+    'Sana',
+    'Chris',
+    'Daim',
+
+};
+let studentID = {1,2,3,4,5};
+let studentCGPA = {3.2,3.1,3.0,2.7,3.0};
+let studentAddress = {"lahore",'peshawar','Islamabad','lahore','lahore'};
+
+console.log(studentNames);
